fd8d2cafe2a7c10504afc86d57e40bbe410d5214
Christalin Dorsey <christalin.dorsey03@gmail.com>
2024-04-25 20:26:29 -0700

    Implement navigation sidebar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,24 +2,64 @@
 
 import { Button } from "../ui/Button"
 import { UserAvatar } from "@/components/NavBar/UserAvatar"
-import { useRouter } from "next/navigation"
 import { useUser } from "@/components/UserProvider"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/Sheet"
+import { usePathname } from "next/navigation"
+import Link from "next/link"
+import { useState } from "react"
+
+const navLinks = [
+  {
+    label: "Home",
+    href: "/home",
+  },
+  {
+    label: "Create Post",
+    href: "/post/submit",
+  },
+]
 
 export function NavBar() {
-  const router = useRouter()
+  const pathname = usePathname()
   const supabaseUser = useUser()
+  const [menuState, setMenuState] = useState(false)
 
   return (
     <div className="backdrop-blur-lg flex h-14 items-center justify-between left-0 px-5 sticky top-0 z-10">
-      <h1 className="cursor-pointer font-extrabold text-xl" onClick={() => router.push("/home")}>🧶 Threadify</h1>
+      <Sheet open={menuState} onOpenChange={(isOpen) => setMenuState(isOpen)}>
+        <SheetContent className="max-sm:w-screen" side="left">
+          <div className="flex flex-col gap-5 h-full items-center justify-center">
+            {navLinks.map((navLink) => (
+              <Link
+                key={navLink.href}
+                href={navLink.href}
+                className={`text-xl ${
+                  pathname.includes(navLink.href)
+                    ? "decoration-4 font-bold text-blue-500 underline underline-offset-4"
+                    : ""
+                }`}
+                onClick={() => setMenuState(false)}
+              >
+                {navLink.label}
+              </Link>
+            ))}
+          </div>
+        </SheetContent>
 
-      <div className="cursor-pointer flex gap-3 items-center justify-center">
-        {supabaseUser ? (
-          <UserAvatar supabaseUser={supabaseUser} />
-        ) : (
-          <Button onClick={() => router.push("/login")}>Log In</Button>
-        )}
-      </div>
+        <SheetTrigger asChild>
+          <h1 className="cursor-pointer font-extrabold text-xl">
+            🧶 Threadify
+          </h1>
+        </SheetTrigger>
+      </Sheet>
+
+      {supabaseUser ? (
+        <UserAvatar supabaseUser={supabaseUser} />
+      ) : (
+        <Link href="/login">
+          <Button>Log In</Button>
+        </Link>
+      )}
     </div>
   )
 }
